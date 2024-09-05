603c8e5c89a365f27e58490fd00a21cec9d82f1c
jasonhwang <junghahwang0601@gmail.com>
2024-09-05 15:02:48 +0900

    비밀번호 찾기 테스트

--- src/main/java/com/hotel/flint/user/employee/controller/EmployeeController.java
+++ src/main/java/com/hotel/flint/user/employee/controller/EmployeeController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.persistence.EntityNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/employee")
@@ -98,8 +97,8 @@ public class EmployeeController {
 //    직원 비밀번호 찾기. 회원과 같은 로직
     public ResponseEntity<?> findPassword(@RequestBody FindPasswordRequest request) {
         Class<?> employee = mailService.sendTempPassword(request);
+        Class<?> member = mailService.sendTempPassword(request);
         if(!(employee==null)){
-            System.out.println(employee);
             try {
                 CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "임시 비밀번호를 이메일로 발송했습니다.", null);
                 return new ResponseEntity<>(commonResDto, HttpStatus.OK);
@@ -110,7 +109,11 @@ public class EmployeeController {
                 CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), "이메일 전송에 실패했습니다.");
                 return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
             }
-        }else{
+        }else if(member==null){
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), "해당 직원은 존재하지 않습니다.");
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
+        }
+        else{
             CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), "해당 직원의 정보가 없습니다.");
             return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
         }
