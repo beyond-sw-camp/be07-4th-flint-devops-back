bc638a842432fe77eb5e57fb923b6d0bc1e1b71a
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-09-05 11:21:33 +0900

    배포 test : log 보기

--- src/main/java/com/hotel/flint/reserve/dining/controller/DiningReservationController.java
+++ src/main/java/com/hotel/flint/reserve/dining/controller/DiningReservationController.java
@@ -81,6 +81,7 @@ public class DiningReservationController {
             List<ReservationListResDto> reservationListResDtos = diningReservationService.userList(pageable);
             log.info(reservationListResDtos.toString());
             CommonResDto commonResDto = new CommonResDto(HttpStatus.OK,  reservationListResDtos.get(0).getMemberId() + "님 예약 조회", reservationListResDtos);
+            log.info("commonResDto" + commonResDto);
             return new ResponseEntity<>( commonResDto, HttpStatus.OK );
         }catch (IllegalArgumentException e) {
             CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), e.getMessage());
